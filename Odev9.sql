e2a61f20adf520640c772eacc7c8f412028eda19
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-04-16 15:15:34 +0300

    Sql Odev 9
    Patika.dev - SQL Path - Odev 9

---
+++ Odev9.sql
@@ -0,0 +1,13 @@
+-- ========== ODEV 9 ==========
+
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city, country FROM city
+JOIN country ON city.country_id = country.country_id;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN
+SELECT payment_id, first_name, last_name FROM payment
+JOIN customer ON customer.customer_id = payment.customer_id;
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN
+SELECT rental_id, first_name, last_name FROM rental
+JOIN customer ON rental.customer_id = customer.customer_id;
